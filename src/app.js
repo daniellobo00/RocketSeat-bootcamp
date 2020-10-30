ad4022a949d1fa0eac5eab8f4c337bf08f682718
daniellobo00 <daniellobo00@hotmail.com>
2020-10-30 15:11:09 -0300

    Correção devido a "uuid"

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require("uuidv4");
+//const { uuid } = require("uuidv4");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cors());
 
